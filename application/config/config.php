c587207f2bc4c7e541dca7329b7874445cf7ddbf
Anh Duc Bui <anhduc130@live.com>
2016-08-03 22:29:03 +0700

    change config

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/web_app';
+$config['base_url'] = 'http://localhost:8888/Web-App-Project';
 
 /*
 |--------------------------------------------------------------------------
