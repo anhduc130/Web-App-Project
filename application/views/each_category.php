5dac75abdd1f9dbfb0eab0690a46bf2d415f927d
Anh Duc Bui <anhduc130@live.com>
2016-09-01 04:38:04 -0700

    Change in each category

--- application/views/each_category.php
+++ application/views/each_category.php
@@ -10,49 +10,49 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>			
 	</div>
 	<br><hr>
 	<div class="row">
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>	
 	</div>
 	<br><hr>
 	<div class="row">
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>	
 	</div>
 	<br><hr>
 	<div class="row">
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>
 		<div class="col-md-4">
-			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto%;">
+			<img src="<?php echo base_url();?>/static/images-QuyenTang/aiesec.png" style="width:100%;height:auto;">
 		</div>	
 	</div>
 </div>
