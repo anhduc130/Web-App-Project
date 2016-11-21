88934a82817f9b6044a5db3b48344294cac174d2
Andrew Bui <anhduc130@live.com>
2016-11-20 17:53:16 -0800

    Fixed aboutUs and logo's responsive issue

--- application/views/_includes/aboutUs.php
+++ application/views/_includes/aboutUs.php
@@ -2,33 +2,32 @@
 	<div class="container">
 		<div class="row border">
 
-			<div class="col-md-8 white-text">
-				<h1 class="text-center">ABOUT US</h1>
-				<p class="transparent text-center">
-					Hi people! <br>
-					We are a group of Software Engineering students who develop this website as a non-profit project for Vietnamese community. We hope to contribute our efforts to make a better life for people in Vietnam, especially students. We are strongly passionate about software creations and technologies. Please support us, bitches!
+			<div class="col-sm-8 col-xs-12 white-text">
+				<h2 class="text-center">ABOUT US</h2>
+				<p class="transparent">
+					We are a group of Software Engineering students who develop this website as a non-profit project for the community. We hope to contribute our efforts to make a better life for people in Vietnam, especially students. We are strongly passionate about software creations and technologies. Please support us!
 				</p>
 			</div>
 
-			<div class="col-md-4">
+			<div class="col-sm-4 col-xs-12">
 				<div class="row margin-bottom"><h3 class="contact-us text-center white-text">CONTACT US</h3></div>
 
 				<div class="row margin-bottom">
-					<div class="col-md-offset-1 text-center">
+					<div class="col-sm-offset-1 col-xs-offset-1 text-center">
 						<img src="<?php echo base_url("static/images/contact-icon/emailS.png"); ?>" alt="">
 						<span class="white-text contact-info">abc@xyz.com</span>
 					</div>					
 				</div>
 				
 				<div class="row margin-bottom">
-					<div class="col-md-offset-1 text-center">
+					<div class="col-sm-offset-1 col-xs-offset-1 text-center">
 						<img src="<?php echo base_url("static/images/contact-icon/fbS.png"); ?>" alt="">
 						<span class="white-text contact-info">abc@xyz.com</span>
 					</div>					
 				</div>
 
 				<div class="row margin-bottom">
-					<div class="col-md-offset-1 text-center">
+					<div class="col-sm-offset-1 col-xs-offset-1 text-center">
 						<img src="<?php echo base_url("static/images/contact-icon/telephoneS.png"); ?>" alt="">
 						<span class="white-text contact-info">abc@xyz.com</span>
 					</div>					
